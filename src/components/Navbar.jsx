f07868229886ede222643f9c0a1b300af75e5dae
mishra-riya06 <mriya0619@gmail.com>
2025-10-30 21:58:30 +0530

    Initial project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+        <div>
+            <img src="./src/assets/Logo.png" alt="" />
+        </div>
+        <div>
+            <h2>Home</h2>
+            <h2>Feature</h2>
+            <h2>Priceing</h2>
+            <h2>Support</h2>
+            <button>Sign up</button>
+            <button>Log in</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
