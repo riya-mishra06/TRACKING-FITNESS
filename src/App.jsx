164d478a19b5037e5ea0022260b3a81c4a466508
mishra-riya06 <mriya0619@gmail.com>
2025-11-02 11:50:02 +0530

    Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import MainHome from './components/MainHome'
 
 const App = () => {
   return (
  <>
- <Navbar/>
+ <MainHome/>
  </>
   )
 }
